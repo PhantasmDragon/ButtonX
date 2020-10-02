324320357f0202652dae418d8a93068c8a9480ed
abstractHero <this.phantasmdragon@gmail.com>
2020-10-02 13:20:31 +0300

    Added SharedPreferences class to store last click time

---
+++ app/src/main/java/io/github/phantasmdragon/buttonx/data/SharedPreferences.kt
@@ -0,0 +1,25 @@
+package io.github.phantasmdragon.buttonx.data
+
+import android.content.Context
+import androidx.core.content.edit
+
+class SharedPreferences(private val context: Context) {
+
+    private val preferences by lazy {
+        context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+    }
+
+    var lastClickTime: Long
+        get() = preferences.getLong(KEY_LAST_CLICK_TIME, System.currentTimeMillis())
+        set(value) {
+            preferences.edit {
+                putLong(KEY_LAST_CLICK_TIME, value)
+            }
+        }
+
+    private companion object {
+        const val FILE_NAME = "SYSTEM"
+        const val KEY_LAST_CLICK_TIME = "KEY_LAST_CLICK_TIME"
+    }
+
+}
